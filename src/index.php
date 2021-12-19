1a4cec61a0f71a15d9b0ce6e52dfa15f1b2939ed
Viktor Szépe <viktor@szepe.net>
2021-12-19 18:02:24 +0000

    Do not follow WPCS

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$wpstarter_context          = Timber::context();
+$wpstarter_context = Timber::context();
 $wpstarter_context['posts'] = new Timber\PostQuery();
 
-Timber::render( 'base.twig', $wpstarter_context );
+Timber::render('base.twig', $wpstarter_context);
--- src/page.php
+++ src/page.php
@@ -1,6 +1,6 @@
 <?php
 
-$wpstarter_context         = Timber::context();
+$wpstarter_context = Timber::context();
 $wpstarter_context['post'] = new Timber\Post();
 
-Timber::render( 'page.twig', $wpstarter_context );
+Timber::render('page.twig', $wpstarter_context);
